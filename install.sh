e7b2a2e793f8720d700edee44ef2f67772c7e40a
pcuinty <ciusubv.g6@dispostable.com>
2016-12-28 21:48:56 +0100

    renamed dotfiles (removed the leading dot), edited install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-ln -sv ~/.dotfiles/.bashrc ~
-ln -sv ~/.dotfiles/.spacemacs ~
-ln -sv ~/.dotfiles/.gitconfig ~
+############################
+# install.sh
+# This script backs up existing dotfiles in a dotfiles_old directory, and creates symlinks to the dotfiles in this repository
+############################
 
+###### Variables
+dir=~/.dotfiles
+olddir=~/dotfiles_old
+files=".bashrc .spacemacs"
+######
+
+# Creating dotfiles_old directory
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to dotfile directory
+echo -n "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old, then create symlinks in homedir for any file specified in $files
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/$olddir
+    echo "Creating symlink to $file in home directory"
+    ln -sv $dir/$file ~/.$file
+done
+echo "...done"
